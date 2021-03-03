d1d7bcfb8accebf22d44be474b630feff561c8e1
stormy747 <andreagondolo02@gmai.com>
2021-03-03 22:24:58 +0100

    views

--- controllers/controllerBasics.js
+++ controllers/controllerBasics.js
@@ -1,10 +1,10 @@
 // export, permette di usare la funzione al di fuori del file
 exports.get_home  = (req, res) => {
     // restituisce la pagina indicata
-    res.status(200).render("./bisarca");
+    res.status(200).render("./esempio");
 }
 
 exports.get_catalogo  = (req, res) => {
     // res.status(200).render("./bisarca");
-    console.log("funzionaaa");
+    console.log("funziona");
 }
