00be0b9beef4608db4de1800a15cbdc539107f53
inf.gondoloa1112 <inf.gondoloa1112@itiscuneo.local>
2021-03-04 10:50:44 +0100

    controllerShoes

---
+++ controllers/controllerShoes.js
@@ -0,0 +1,11 @@
+// 04/03/2021 lsti things
+
+const express = require('express');
+const require = require('../models/Scarpe');
+
+const app = express;
+
+// prendo la pagina 
+exports.get = (req, res) => {
+
+}
