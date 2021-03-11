adf877ed839788938f81dcd3ac3a43d69b7a20b7
samueleGiraudo02 <samuelegiraudo@itiscuneo.eu>
2021-03-11 09:30:29 +0100

    navbar

---
+++ models/Utenti.js
@@ -0,0 +1,35 @@
+const { Double } = require('mongodb');
+const mongoose = require ('mongoose');
+
+var schema = new mongoose.Schema({
+    id: {
+        type: String,
+        //se obbligatorio o meno
+        required: true
+    },
+
+    nome: {
+        type: String,
+        required: true
+    },
+
+    cognome: {
+        type: String,
+        required: true
+    },
+
+    indirizzo: {
+        type: String,
+        required: true
+    },
+
+    password: {
+        type: Double,
+        required: true
+    }
+});
+
+// copia dello shcema
+var Post = mongoose.model('Utenti', schema);
+// esporto il modulo per usarlo nei controller
+module.exports = Utenti;
