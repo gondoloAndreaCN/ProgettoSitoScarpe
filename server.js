02a52a915a8d36254a72d86eab7411ea6d46d80d
stormy747 <andreagondolo02@gmai.com>
2021-03-18 12:52:06 +0100

    conncetion DB

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const mongoose = require ("mongoose");
 const app = require("./app.js");
+const mongo = require("./connectionDB");
+
+mongo.init();
 
 /* Definizione porta di accesso in locale */
 const PORT = 8080 ;
@@ -7,21 +10,4 @@ app.listen(PORT, () => {
   console.log(
     `Scraping Scarpe | Server started on port ${PORT} - use 'localhost:${PORT}`
   );
-});
-
-// server richiede l'app.js
-let db = require('./config/keys').MongoURI;
-// si connette al db
-mongoose.connect(db, {
-  useUnifiedTopology: true,
-  useNewUrlParser: true
-});
-
-// riporta lo stato della connessione
-db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-
-/* Se la connessione è andata a buon fine loggiamo a console il messaggio */
-db.on('open', () => {
-  console.log('Scraping Scarpe | MongoDB Connected');
 });
